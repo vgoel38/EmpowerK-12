cfe5578c96ca493a04ec4546b7536126f0159785
Vishal Goel <vgoel38@gmail.com>
2016-04-13 00:28:11 +0500

    removed localhost ip

--- application.py
+++ application.py
@@ -245,6 +245,7 @@ def showMemoryVideos():
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+    #app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    app.run()
