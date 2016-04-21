8e83e75ca2c53705c823036afb451f7c3dc1af64
Vishal Goel <vgoel38@gmail.com>
2016-04-22 00:03:00 +0500

    application.run() commented

--- application.py
+++ application.py
@@ -254,4 +254,4 @@ def showCourses():
 if __name__ == '__main__':
     application.secret_key = 'super_secret_key'
     application.debug = True
-    application.run(host='0.0.0.0', port=5000)
+    # application.run(host='0.0.0.0', port=5000)
